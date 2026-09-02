02064543b9346a3487ab11ab86905710f5d5e0cb
unknown <khalilinassim85@gmail.com>
2026-09-02 11:59:05 +0330

    Add authentication API service

---
+++ frontend/src/services/auth.ts
@@ -0,0 +1,18 @@
+import api from "./api";
+
+interface LoginResponse {
+  access: string;
+  refresh: string;
+}
+
+export const login = async (
+  username: string,
+  password: string
+): Promise<LoginResponse> => {
+  const response = await api.post<LoginResponse>("/token/", {
+    username,
+    password,
+  });
+
+  return response.data;
+};
